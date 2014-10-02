18f5d3d5b47b4c057dcab956b3cc8a5451bfbab7
Tai LE <tai.le@infonam.com>
2014-10-02 14:37:32 +0700

    'init'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RDeport::Application.config.secret_token = 'cb7d969e148bcde4c5e4052968df5baa20a0d501c1d3c532a882e6f4d61cc30fc998b2f1b750ac467b2e83dddcb1c377e45970eafe99a71926c73e19ccf97218'
